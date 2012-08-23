e0a9f4adae506541ba0605e8403edbca51174fef
tanaka yohei <uroboros.5342.bung.bonber@ezweb.ne.jp>
2012-08-23 16:24:05 +0900

    エンディング、オープニング追加

--- scenes/ending.rb
+++ scenes/ending.rb
@@ -1,13 +1,41 @@
 # coding: utf-8
-
+require 'dxruby' 
 class Ending
-  def initialize
-    bg_file = File.join(File.dirname(__FILE__), "..", "images", "ending_bg.png")
+ def initialize
+    bg_file = File.join(File.dirname(__FILE__), "..", "images", "ending_back.png")
+   
+		sound_file = File.join(File.dirname(__FILE__), "..", "sounds", "jingle-090610-fanfare01-wav.wav")
     @background_img = Image.load(bg_file)
-
+		@sound = Sound.new(sound_file)
+		@score1 = 2
+		@score2 = 1
+		@x = 0
+		
     @title_font = Font.new(64, "ＭＳ ゴシック", weight: true)
-    @title_str = "ゲームクリア！！"
+    @title_str = "ゲーム終了！！"
     @title_x = Util.get_center(@title_str, @title_font)
+
+    @result_font = Font.new(34, "ＭＳ ゴシック", weight: true)
+    @result_str = "～結果～"
+   
+
+		
+    @result2_font = Font.new(34, "ＭＳ ゴシック", weight: true)
+    @result2_str = "プレイヤー1：#{@score1}"
+    
+		@result3_font = Font.new(34, "ＭＳ ゴシック", weight: true)
+    @result3_str = "プレイヤー2：#{@score2}"
+   
+		@result4_font = Font.new(44, "ＭＳ ゴシック", weight: true)
+    @result4_str = "～勝者～"
+
+		@result5_font = Font.new(44, "ＭＳ ゴシック", weight: true)
+    @result5_str = "プレイヤー１ おめでとう!!"
+
+		@result6_font = Font.new(44, "ＭＳ ゴシック", weight: true)
+    @result6_str = "プレイヤー２ おめでとう!!"
+
+
 
     @description_font = Font.new(24, "ＭＳ ゴシック", weight: true)
     @description_str = "リターンキーを押して終了します"
@@ -16,11 +44,26 @@ class Ending
 
   def play
     Window.draw(0, 0, @background_img)
-    Window.drawFont(@title_x, 10, @title_str, @title_font, color: Util::YELLOW)
-    Window.drawFont(@description_x, 400, @description_str, @description_font, color: Util::YELLOW)
+    Window.drawFont(@title_x, 60, @title_str, @title_font, color: Util::YELLOW)
+    Window.drawFont(260, 200, @result_str, @result_font, color: Util::YELLOW)
+  	Window.drawFont(250, 250, @result2_str, @result2_font, color: Util::YELLOW)
+    Window.drawFont(250, 320, @result3_str, @result3_font, color: Util::YELLOW)
+		Window.drawFont(170, 430, @result4_str, @result4_font, color: Util::RED)
+		
+		if @score1 > @score2
+					Window.drawFont(170, 500, @result5_str, @result5_font, color: Util::RED)
+		else
+					Window.drawFont(170, 500, @result6_str, @result6_font, color: Util::RED)
+		end
 
-    if Input.keyPush?(K_RETURN)
-      exit
-    end
+		if @x == 0
+  		 @sound.play
+     	 @x = 1
+		end
+		
+	
+		if Input.keyPush?(K_RETURN)
+			exit
+		end
   end
 end
--- scenes/title.rb
+++ scenes/title.rb
@@ -1,28 +1,54 @@
 # coding: utf-8
 
-require "#{File.dirname(__FILE__)}/game/event"
-
 class Title
   def initialize
-    bg_file = File.join(File.dirname(__FILE__), "..", "images", "title_bg.png")
+    bg_file = File.join(File.dirname(__FILE__), "..", "images", "opening.png")
+    bg2_file = File.join(File.dirname(__FILE__), "..", "images", "logo.png")
+    sound_file = File.join(File.dirname(__FILE__), "..", "sounds", "fanfare.wav")
     @background_img = Image.load(bg_file)
+    @logo_img = Image.load(bg2_file)
+    @sound = Sound.new(sound_file)
+		@sound.setVolume(250, 0)
+    @x = 0
 
-    @title_font = Font.new(64, "ＭＳ ゴシック", weight: true)
-    @title_str = "サンプルボードゲーム"
-    @title_x = Util.get_center(@title_str, @title_font)
+   	@sub_title_font = Font.new(29, "ＭＳ ゴシック", weight: true)
+    @sub_title_str = "シマネクエスト"
+    
 
-    @description_font = Font.new(24, "ＭＳ ゴシック", weight: true)
-    @description_str = "スペースキーを押下して開始してください"
-    @description_x = Util.get_center(@description_str, @description_font)
-  end
+		
+    @description_font = Font.new(32, "ＭＳ ゴシック", weight: true)
+    @description_str = "～ルール～"
+    
+    @description2_font = Font.new(29, "ＭＳ ゴシック", weight: true)
+    @description2_str = "サイコロをまわして、島根の観光名所を巡ろう!!"
+    
+
+    @description3_font = Font.new(29, "ＭＳ ゴシック", weight: true)
+    @description3_str = "たくさん訪れた方が勝ちです."
+    
+    @description4_font = Font.new(30, "ＭＳ ゴシック", weight: true)
+    @description4_str = "スペースキーを押したら、ゲームをはじめます."
+end
 
   def play
     Window.draw(0, 0, @background_img)
-    Window.drawFont(@title_x, 10, @title_str, @title_font, color: Util::YELLOW)
-    Window.drawFont(@description_x, 400, @description_str, @description_font, color: Util::YELLOW)
+    Window.draw(100, 130, @logo_img)
 
-    if Input.keyPush?(K_SPACE)
-      Scene.set_scene(:game)
+ 
+    Window.drawFont(420, 280, @sub_title_str, @sub_title_font, color: Util::RED)
+		Window.drawFont(200, 380, @description_str, @description_font, color: Util::YELLOW)
+    Window.drawFont(200, 420, @description2_str, @description2_font, color: Util::YELLOW)
+    Window.drawFont(200, 460, @description3_str, @description3_font, color: Util::YELLOW)
+    Window.drawFont(170, 700, @description4_str, @description4_font, color: Util::RED)
+    
+    if @x == 0
+       @sound.play
+       @x = 1
+    end
+
+   if Input.keyPush?(K_SPACE)
+        @sound.stop
+				Scene.set_scene(:game)
     end
   end
 end
